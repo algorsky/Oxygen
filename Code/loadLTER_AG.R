f5deddd3b6b1a0b463b2ea29d37cdf504942928c
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro.local>
2020-06-10 12:39:42 -0500

    update

--- Code/loadLTER_AG.R
+++ Code/loadLTER_AG.R
@@ -11,5 +11,19 @@ infile1 <- tempfile()
 download.file(inUrl1,infile1,method="curl")
 
 ice = read_csv(infile1) 
-str(ice) # Always check structure to make sure classes are correct
+str(ice) # Check Structure
+#Filter for northern lakes
+ice = ice %>% filter(lakeid %in% c('AL','CB','CR','SP','TB','TR','BM'))
+write.csv(ice,"/Users/adriannagorsky/Documents/Research/Oxygen/Oxygen/Data/ice.csv", row.names = FALSE)
 
+
+# Package ID: knb-lter-ntl.301.1 Cataloging System:https://pasta.lternet.edu.
+# Data set title: North Temperate Lakes LTER Morphometry and Hypsometry data for core study lakes.
+
+inUrl2  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/301/1/a6905b8cdf899fe706972eee9cdfeffb" 
+infile2 <- tempfile()
+download.file(inUrl2,infile2,method="curl")
+
+hypso = read_csv(infile2)
+str(hypso)
+hypso = hypso %>% filter(lakeid %in% c('AL','CB','CR','SP','TB','TR','BM')) 
