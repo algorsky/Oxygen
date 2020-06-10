b565bb3e6db08bba739ec0a58525363cebedd7a5
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro.local>
2020-06-10 14:45:43 -0500

    update

--- Code/JoinOxygendata.R
+++ Code/JoinOxygendata.R
@@ -6,10 +6,14 @@ library(lubridate)
 ## Load the data from Github and check structure
 hypo <- read_csv('Data/hypso.csv')
 str(hypo)
+hypo<- hypo %>% select(c(lakeid, depth, volume))
 ice <- read_csv('Data/ice.csv')
 str(ice)
 oxygen <- read_csv('Data/o2_data_cleaned.csv')
 str(oxygen)
 oxygen<- oxygen %>% select(lakeid:o2sat)%>%
-  rename(year = year4) %>% left_join(oxygen, hypo, by = "depth") %>% 
-  left_join(ice, by = "year") 
+  rename(year = year4) 
+
+oxygenjoin <- left_join(oxygen, hypo, by = c("lakeid" = "lakeid", "depth" = "depth")) %>% 
+  left_join(ice, by = c("lakeid" = "lakeid", "year" = "year")) 
+str(oxygenjoin)
