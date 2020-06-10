bd89f190855e4a673e445fe540b9fec3dd198761
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Pro.local>
2020-06-10 15:20:50 -0500

    update

--- Code/JoinOxygendata.R
+++ Code/JoinOxygendata.R
@@ -11,9 +11,20 @@ ice <- read_csv('Data/ice.csv')
 str(ice)
 oxygen <- read_csv('Data/o2_data_cleaned.csv')
 str(oxygen)
+#Remove unnecessary variables
 oxygen<- oxygen %>% select(lakeid:o2sat)%>%
   rename(year = year4) 
 
+#Join datasets
 oxygenjoin <- left_join(oxygen, hypo, by = c("lakeid" = "lakeid", "depth" = "depth")) %>% 
-  left_join(ice, by = c("lakeid" = "lakeid", "year" = "year")) 
-str(oxygenjoin)
+  left_join(ice, by = c("lakeid" = "lakeid", "year" = "year")) %>%
+#Add hydro year column
+  mutate(hydroyear = 
+           as.Date(ifelse(month(sampledate)<=10, sampledate, sampledate+years(1)),origin = "1970-01-01")) 
+
+#Work with just Winter dataset
+winteroxy<- oxygenjoin %>%
+  filter(hydroyear >= datefirstice | hydroyear <= datefirstopen) %>%
+#Calculate Hypsometrically Weighted o2 rates
+  mutate("multipliedVol" = volume * o2) 
+
