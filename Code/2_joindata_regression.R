73297107559b24cc492151dfbfe22dc39dc77f96
hdugan <hilarydugan@gmail.com>
2020-06-12 15:45:41 -0500

    grouping and plotting

--- Code/2_joindata_regression.R
+++ Code/2_joindata_regression.R
@@ -37,12 +37,37 @@ winteroxy<- oxygenjoin %>%
   mutate(lastdays = 
          as.numeric(ifelse((month(sampledate)==11|month(sampledate)==12), sampledate - datefirstice, sampledate-yearfirstice))) 
 
+# Probably best practice to group and summarise so you're not plotting multiple of the samepoints
+# You might want to keep different columns for different reasons, but simple for now
+winteroxy_grp = winteroxy %>% 
+  group_by(lakeid, sampledate) %>%
+  summarise(oxygenMass = mean(oxygenMass), lastdays = mean(lastdays), hydroyear = mean(hydroyear))
+
 # Plot regressions
-winteroxy%>%
-  filter(lakeid == "TB")%>%
+winteroxy_grp %>%
+  filter(lakeid == "TB") %>%
   ggplot() + geom_point(aes(x = lastdays, y = oxygenMass)) +
   geom_smooth(aes(x = lastdays, y = oxygenMass), method = lm) +
   facet_wrap(~year(hydroyear))
+# You're getting that error because there are some years with only two values
+
+# HD: All regressions together
+winteroxy_grp %>%
+  filter(lakeid == "TB") %>%
+  ggplot(aes(x = lastdays, y = oxygenMass, color = year(hydroyear), group = year(hydroyear))) + 
+  geom_point() +
+  geom_smooth(method = lm, alpha = 0.1) +
+  scale_colour_viridis_c(name = 'year') +
+  facet_wrap(~lakeid)
+
+# HD: All regressions together, free scales
+winteroxy_grp %>%
+  # filter(lakeid == "TB") %>%
+  ggplot(aes(x = lastdays, y = oxygenMass, color = year(hydroyear), group = year(hydroyear))) + 
+  geom_point(alpha = 0.6) +
+  geom_smooth(method = lm, se = F, alpha = 0.1) + # Turn SE off
+  scale_colour_viridis_c(name = 'year') +
+  facet_wrap(~lakeid, scales = 'free')
 
 #Multiple Regressions
 library(broom)
@@ -52,8 +77,8 @@ winteroxy <- winteroxy %>%
   mutate(hydro = year(hydroyear))
 
 #Run Multiple Regressions
-regression<- winteroxy %>% 
-  filter(lakeid == "TB")%>%
+regression <- winteroxy %>% 
+  filter(lakeid == "TB") %>%
   nest(-(hydro))%>% 
   mutate(
     fit = map(data, ~ lm(oxygenMass ~ lastdays, data = .x)),
