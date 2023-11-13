be4ca2adb2c39702489cf9bc4d5d9ef067a62b06
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Air.local>
2023-11-13 14:10:39 -0600

    updated data

--- Code/load_DOC.R
+++ Code/load_DOC.R
@@ -13,21 +13,21 @@ library(zoo)
 # Contact:  NTL Lead PI -  University of Wisconsin  - ntl.leadpi@gmail.com
 # Stylesheet v2.11 for metadata conversion into program: John H. Porter, Univ. Virginia, jporter@virginia.edu 
 
-inUrl1  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/1/52/802d63a4c35050b09ef6d1e7da3efd3f" 
+inUrl1  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/1/60/0ff1fd13116d6097376e3745194cdc5f" 
 infile1 <- tempfile()
 download.file(inUrl1,infile1,method="curl")
 
 # This works, but can also use the code below to specify column names.
-dt2 = read_csv(infile1) 
-str(dt2) # Always check structure to make sure classes are correct
+doc_dt2 = read_csv(infile1) 
+str(doc_dt2) # Always check structure to make sure classes are correct
 
 # Filter for northern lakes
-dt2 = dt2 %>% filter(lakeid %in% c('AL','CB','CR','SP','TB','TR','BM'))
-dt3<- dt2[c(1:8,14)] #Only keep important columns
+doc_dt2 = doc_dt2 %>% filter(lakeid %in% c('AL','CB','CR','SP','TB','TR','BM'))
+doc_dt3<- doc_dt2[c(1:8,14)] #Only keep important columns
 
 # Check for duplicate sampling of oxygen (first filter NAs, then group by lake/date/depth)
 #Also filter out -99.00 values
-data<-dt3 %>% 
+data<-doc_dt3 %>% 
   filter(!is.na(doc))
 data<-data %>% 
   filter(doc != -99.00)
@@ -40,4 +40,9 @@ avgData <- data%>%
 write.csv(avgData,"Data/doc_data_cleaned.csv", row.names = FALSE)
 
 
-
+ggplot(data = avgData, aes(x = sampledate, y = mean_DOC, shape = lakeid, color = lakeid))+
+  geom_point()+
+  xlab("")+
+  ylab("Dissolved Organic Carbon (mg/L)")+
+  theme_bw()+
+  theme(legend.position = "none")
